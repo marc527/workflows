e18f3da9ec8c297474582bee946887bad6cf648e
Marc Huang <yu.huang.151@gmail.com>
2017-03-25 18:52:27 +1100

    add automation

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,25 @@
 var gulp = require('gulp'),
   gutil = require('gulp-util'),
   coffee = require('gulp-coffee'),
-  concat = require('gulp-concat')
-  browserify = require('gulp-browserify')
+  concat = require('gulp-concat'),
+  browserify = require('gulp-browserify'),
+  connect = require('gulp-connect'),
   compass = require('gulp-compass');
 
-var coffeeSources = ['components/coffee/*.coffee'];
-var jsSources = ['components/scripts/*.js'];
-var sassSources = ['components/sass/style.scss'];
+var env, coffeeSources, jsSources, sassSources, htmlSources, jsonSources, outputDir;
+
+env= process.env.NODE.ENV || 'development';
+if(env === 'development') {
+  outputDir = 'builds/development/';
+} else {
+  outputDir = 'builds/production/';
+}
+
+coffeeSources = ['components/coffee/*.coffee'];
+jsSources = ['components/scripts/*.js'];
+sassSources = ['components/sass/style.scss'];
+htmlSources = [outputDir + '*.html'];
+jsonSources = [outputDir + 'js/*.json'];
 
 gulp.task('coffee', function(){
    gulp.src(coffeeSources)
@@ -20,24 +32,46 @@ gulp.task('js', function(){
   gulp.src(jsSources)
     .pipe(concat('script.js'))
     .pipe(browserify())
-    .pipe(gulp.dest('builds/development/js'))
+    .pipe(gulp.dest(outputDir + 'js'))
+    .pipe(connect.reload())
 });
 
 gulp.task('compass', function(){
   gulp.src(sassSources)
     .pipe(compass({
+      css: outputDir + 'css',
       sass: 'components/sass',
-      image: 'builds/development/images',
+      image: outputDir + 'images',
       style: 'expanded'
     })
     .on('error', gutil.log))
-    .pipe(gulp.dest('builds/development/css'))
+    .pipe(gulp.dest(outputDir + 'css'))
+    .pipe(connect.reload())
 });
 
 gulp.task('watch', function(){
   gulp.watch(coffeeSources, ['coffee']);
   gulp.watch(jsSources, ['js']);
   gulp.watch('components/sass/*.scss', ['compass']);
+  gulp.watch(htmlSources, ['html']);
+  gulp.watch(jsonSources, ['json']);
 });
 
-gulp.task('default', ['coffee', 'js', 'compass', 'watch']);
+gulp.task('connect', function(){
+  connect.server({
+    root: outputDir,
+    livereload: true
+  });
+});
+
+gulp.task('html', function(){
+  gulp.src(htmlSources)
+    .pipe(connect.reload())
+});
+
+gulp.task('json', function(){
+  gulp.src(jsonSources)
+    .pipe(connect.reload())
+});
+
+gulp.task('default', ['coffee', 'js', 'compass', 'connect', 'watch']);
