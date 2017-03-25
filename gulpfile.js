1c4730dad6f37439bee7dd660f53d7f844c34a37
Marc Huang <yu.huang.151@gmail.com>
2017-03-25 19:07:19 +1100

    add gulp if and uglify

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,9 @@ var gulp = require('gulp'),
   concat = require('gulp-concat'),
   browserify = require('gulp-browserify'),
   connect = require('gulp-connect'),
-  compass = require('gulp-compass');
+  compass = require('gulp-compass')
+  gulpif = require('gulp-if'),
+  uglify = require('gulp-uglify');
 
 var env, coffeeSources, jsSources, sassSources, htmlSources, jsonSources, outputDir, sassStyle;
 
@@ -34,6 +36,7 @@ gulp.task('js', function(){
   gulp.src(jsSources)
     .pipe(concat('script.js'))
     .pipe(browserify())
+    .pipe(gulpif(env === 'production', uglify()))
     .pipe(gulp.dest(outputDir + 'js'))
     .pipe(connect.reload())
 });
